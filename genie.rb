b2dcbffc0538f346f52e804b722e7180e2d167b6
kris <ksoule77@gmail.com>
2017-08-17 22:27:32 -0500

    work on lamp entrance

--- genie.rb
+++ genie.rb
@@ -1,10 +1,51 @@
 class Genie
-  def initialize(name, age)
-    p "#{name} is a #{age} year old genie."
+  lamp state
+
+
+
+  def enter_lamp
+    if in_lamp == true
+      p "#{@named} moves to the den."
+    else
+      p "#{named} enters the lamp in a puff of steam."
+    return in_lamp == true
+    end
+  end
+
+  def exit_lamp
+    if in_lamp == false
+      p "#{named} appears from behind a nearby bush and yells surprise"
+    else
+      p "#{named} exits the lamp in a grape scented vape cloud"
+    return in_lamp == false
+    end
+  end
+
+  def age
+      @age
+  end
+
+  def named=(new)
+      @named
+  end
+
+  def initialize(age, named)
+    p "this is the bottle of a genie who is #{age} years old, by the name of #{named}"
+    @age = age
+    @named = named
+    p @named
+    p @age
+    in_lamp = true
+  end
+
+  def gpp(new_name)
+
+    p "to protect the guilty and injure the innocent, #{@named} is now known as #{new_name}"
+    @named = new_name
+    p "#{@named} is #{age}"
   end
 end
-haji.Genie.new
-
-
-
+haji = Genie.new(435, "Haji")
+haji.gpp("Steve")
+haji.enter_lamp
 
